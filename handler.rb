06c18cdaaab5e7f8aa6df0dc2dbc89bc0e250551
higeojisan <makotokoda82@gmail.com>
2019-04-22 20:07:51 +0900

    署名の検証の追加

--- handler.rb
+++ handler.rb
@@ -2,12 +2,35 @@ load "vendor/bundle/bundler/setup.rb"
 
 require 'json'
 require 'line/bot'
+require 'logger'
 
 def input(event:, context:)
+  logger = Logger.new(STDOUT)
+
+  ## クライアントの作成
   client = Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
 
-  p client
+  ## 署名の検証
+  unless client.validate_signature(event["body"], event["headers"]["X-Line-Signature"])
+    logger.fatal("failed to validate signature.") 
+    return 0
+  end
+  
+  events = client.parse_events_from(event["body"])
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      end
+    end
+  }
 end
