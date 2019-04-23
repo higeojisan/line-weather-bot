3385c3d877bbf823d34f436dd1790d6a9416f294
higeojisan <makotokoda82@gmail.com>
2019-04-23 19:16:06 +0900

    入力による分岐を追加

--- handler.rb
+++ handler.rb
@@ -43,8 +43,26 @@ def input(event:, context:)
         ## 一致しない：やり直し
         ## 一致する：地域をサジェストする
         ## ["道北", "道東", "道南", "道央", "道南", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+        ## 北海道は特別扱い
+        if prefecture === '北海道'
+          message = {
+            type: 'text',
+            text: '北海道の方は"道北", "道東", "道南", "道央", "道南"のいずれかを入力してください'
+          }
+        elsif prefectures.grep(/^#{prefecture}/).empty?
+          message = {
+            type: 'text',
+            text: "一致する地域が見つかりませんでした。\n都道府県名を入力してください。"
+          }
+        else
+          message = {
+            type: 'text',
+            text: '確認テンプレートを出す'
+          }
+        end
 
-        #client.reply_message(event['replyToken'], message)
+        response = client.reply_message(event['replyToken'], message)
+        p response
       end
     end
   }
