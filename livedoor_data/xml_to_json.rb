93c04d40de71163c2e5e46fc093cc906bd8b06a0
higeojisan <makotokoda82@gmail.com>
2019-04-26 22:06:25 +0900

    お天気Webサービスの全国の地点定義表をJSONに変換する関数の追加

---
+++ livedoor_data/xml_to_json.rb
@@ -0,0 +1,15 @@
+require 'rexml/document'
+require 'json'
+
+results = {}
+
+rexml_doc = REXML::Document.new(File.open("primary_area.xml").read)
+rexml_doc.elements.each("//ldWeather:source/*") do |elem|
+  elem.elements.each("//pref[@title='#{elem.attribute("title").value}')]/city") do |city|
+    results[:"#{city.attribute('id').value}"] = {city_name: city.attribute("title").value, pref_name: elem.attribute("title").value}
+  end
+end
+
+File.open("primary_area.json", "w") do |f|
+  f.write JSON.pretty_generate(results)
+end
