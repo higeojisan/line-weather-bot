a766521d9125102b39c926aaf20256ce6e0ac5e4
higeojisan <makotokoda82@gmail.com>
2019-04-29 17:56:13 +0900

    設定確認時の動作のリファクタリング

--- handler.rb
+++ handler.rb
@@ -35,48 +35,23 @@ def input(event:, context:)
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
+        
         ## リッチメニューで設定確認をタップされた時の処理
         if event.message['text'] == '設定地域の確認'
           ## user_idを取得してその人のcity_idを取得する
-          ## TODO: 毎回LIVEDOORにアクセスしたくないからcity_idを一覧を永続化したい
-          ## →jsonファイル化した(livedoor_data/primary_area.json)
-          user_id = event['source']['userId']
-          digested_user_id = Digest::SHA256.hexdigest("#{user_id}")
-          s3_object = Aws::S3::Object.new(ENV['USER_INFO_BUCKET'], "#{digested_user_id}_info.csv")
-          if s3_object.exists?
-            s3_object_data = s3_object.get
-            ## TODO: S3のパースはpush.rbにもあるので関数化する
-            CSV.parse(s3_object_data.body.read, headers: true) do |row|
-              ## 念のためuser_idが一致するか確認...一致しないケースあるの？って感じだけど
-              if user_id == row['user_id']
-                city_id = row['city_id']
-                File.open(LIVEDOOR_JSON_FILE) do |file|
-                  city_hash = JSON.load(file)["#{city_id}"]
-                  message = {
-                    type: 'text',
-                    text: "あなたの設定地域は\n#{city_hash['city_name']}(#{city_hash['pref_name']})だよ"
-                  }
-                  response = client.reply_message(event['replyToken'], message)
-                  p response
-                end
-                return
-              else
-                message = {
-                  type: 'text',
-                  text: "設定地域を取得出来ませんでした"
-                }
-                response = client.reply_message(event['replyToken'], message)
-                p response
-                return
-              end
+          digested_user_id = Digest::SHA256.hexdigest("#{event['source']['userId']}")
+          s3_client = Aws::S3::Client.new
+          user_id, city_id = get_user_id_and_city_id_from_s3_obj(s3_client, ENV["USER_INFO_BUCKET"], "#{digested_user_id}_info.csv")
+          if user_id == event['source']['userId']
+            File.open(LIVEDOOR_JSON_FILE) do |file|
+              city_hash = JSON.load(file)["#{city_id}"]
+              message = {
+                type: 'text',
+                text: "あなたの設定地域は\n#{city_hash['city_name']}(#{city_hash['pref_name']})だよ"
+              }
+              response = client.reply_message(event['replyToken'], message)
+              p response
             end
-          else
-            message = {
-              type: 'text',
-              text: "設定地域を取得出来ませんでした"
-            }
-            response = client.reply_message(event['replyToken'], message)
-            p response
           end
           return
         end
