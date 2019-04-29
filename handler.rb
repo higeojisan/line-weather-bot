524496ad307e6ca375fe3ad16636edba96a6bfb5
higeojisan <makotokoda82@gmail.com>
2019-04-29 15:20:35 +0900

    設定地域の確認機能の追加

--- handler.rb
+++ handler.rb
@@ -8,9 +8,14 @@ require 'oga'
 require 'aws-sdk'
 require 'digest/sha2'
 require 'tempfile'
+require 'csv'
+
+LIVEDOOR_JSON_FILE='livedoor_data/primary_area.json'
 
 def input(event:, context:)
   logger = Logger.new(STDOUT)
+
+  p event
 
   ## クライアントの作成
   client = Line::Bot::Client.new { |config|
@@ -30,6 +35,52 @@ def input(event:, context:)
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
+        ## リッチメニューで設定確認をタップされた時の処理
+        if event.message['text'] == '設定地域の確認'
+          ## user_idを取得してその人のcity_idを取得する
+          ## TODO: 毎回LIVEDOORにアクセスしたくないからcity_idを一覧を永続化したい
+          ## →jsonファイル化した(livedoor_data/primary_area.json)
+          user_id = event['source']['userId']
+          digested_user_id = Digest::SHA256.hexdigest("#{user_id}")
+          s3_object = Aws::S3::Object.new(ENV['USER_INFO_BUCKET'], "#{digested_user_id}_info.csv")
+          if s3_object.exists?
+            s3_object_data = s3_object.get
+            ## TODO: S3のパースはpush.rbにもあるので関数化する
+            CSV.parse(s3_object_data.body.read, headers: true) do |row|
+              ## 念のためuser_idが一致するか確認...一致しないケースあるの？って感じだけど
+              if user_id == row['user_id']
+                city_id = row['city_id']
+                File.open(LIVEDOOR_JSON_FILE) do |file|
+                  city_hash = JSON.load(file)["#{city_id}"]
+                  message = {
+                    type: 'text',
+                    text: "あなたの設定地域は\n#{city_hash['city_name']}(#{city_hash['pref_name']})だよ"
+                  }
+                  response = client.reply_message(event['replyToken'], message)
+                  p response
+                end
+                return
+              else
+                message = {
+                  type: 'text',
+                  text: "設定地域を取得出来ませんでした"
+                }
+                response = client.reply_message(event['replyToken'], message)
+                p response
+                return
+              end
+            end
+          else
+            message = {
+              type: 'text',
+              text: "設定地域を取得出来ませんでした"
+            }
+            response = client.reply_message(event['replyToken'], message)
+            p response
+          end
+          return
+        end
+
         prefecture = event.message['text']
         
         ## LIVEDOORのRSSを取得
@@ -52,7 +103,7 @@ def input(event:, context:)
             type: 'text',
             text: '北海道の方は"道北", "道東", "道南", "道央", "道南"のいずれかを入力してください'
           }
-        elsif prefectures.grep(/^#{prefecture}/).empty?
+        elsif prefectures.grep(/^#{prefecture}/).empty? ## TODO: grepだと福だけ入力したら福島もしくは福井が引っかかってしまう...ので修正
           message = {
             type: 'text',
             text: "一致する地域が見つかりませんでした。\n都道府県名を入力してください。"
